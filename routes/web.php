1bc0804a8accb31e289afd4c29a0ccc353820cd8
hicaroos <hicaro9182@gmail.com>
2026-06-20 00:24:02 -0300

    feat: register resource routes for residents, medications, and documents

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,12 @@
 <?php
 
+use App\Http\Controllers\DocumentsController;
+use App\Http\Controllers\MedicationsController;
+use App\Http\Controllers\ResidentsController;
 use Illuminate\Support\Facades\Route;
 
 Route::inertia('/', 'Welcome')->name('home');
+
+Route::resource('residents', ResidentsController::class);
+Route::resource('medications', MedicationsController::class);
+Route::resource('documents', DocumentsController::class);
